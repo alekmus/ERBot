e0020952210b41796b91361ba67d91567eb2b57e
Aleksi <>
2020-01-04 16:52:33 +0200

    Added functionality to create countvectors based on pdf files

--- boersebot/modelbuilder.py
+++ boersebot/modelbuilder.py
@@ -1,9 +1,45 @@
 from boersebot import pdf_reader
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+
+
+def tokenize_string(input_string):
+    """
+    Tokenizes a string and removes stopwords.
+    :param input_string: String to tokenize.
+    :return: A list of tokens for the input_string.
+    """
+    # Collect stopwords and cast the list into a set for faster access
+    stopwords = set(nltk.corpus.stopwords.words('finnish'))
+
+    # Break the input down to words by splitting on whitespace characters
+    words = input_string.split()
+
+    # Initialize a stemmer to reduce the word space in classification
+    stemmer = nltk.stem.snowball.SnowballStemmer('finnish')
+
+    # Stem and collect the actual words (excluding numbers and special characters) that are not stopwords
+    return [stemmer.stem(word) for word in words if (word not in stopwords) and word.isalpha()]
+
+
+def tokenize_samples(samples_location):
+    """
+    Helper function for building a model with naive bayes classifier.
+    Tokenizes all samples in given directory and returns a list containing their tokens.
+    NOTICE: returns a list where the tokens are connected into a string separated by spaces so it's easier to
+    handle for a scipy vectorizer.
+    :param samples_location: Location of samples.
+    :return: List of tokens for each sample.
+    """
+    # Calls tokenize_string on the string representation of each pdf file in the folder and
+    # collects their tokens into a list.
+    # Disregards the text file that contains their labels which is also stored in the directory.
+    return [" ".join(tokenize_string(pdf_reader.pdf_to_string(file.path)))
+            for file in os.scandir(samples_location)
+            if file.name != "labels.csv"]
+
 
 if __name__ == '__main__':
-    sample = pdf_reader.pdf_to_string("samples/0.pdf", 17)
-    lines = sample.split('\n')
-
-    for line in lines:
-        if 'tulos' in line.lower():
-            print(line.strip())
+    cv = CountVectorizer()
+    print(cv.fit_transform(tokenize_samples("samples")).toarray())
--- boersebot/pdf_reader.py
+++ boersebot/pdf_reader.py
@@ -1,14 +1,13 @@
-from pathlib import Path
 import subprocess
 
 
-def pdf_to_string(file_path, page):
+def pdf_page_to_string(file_path, page):
     """
     Translates a page in a pdf file to text. Utilizes Xpdf.
     NOTICE: Because of the pdf format page index starts at 1.
     :param file_path: Path to the pdf file
     :param page: The page that will be translated
-    :return: String containing the text in the pdf file
+    :return: String containing the text on the give page in the pdf file
     """
     # Casts the page parameter to a string so integers can be used when calling the function
     page = str(page)
@@ -24,7 +23,33 @@ def pdf_to_string(file_path, page):
                             stderr=subprocess.PIPE).communicate()[0]
 
 
+def pdf_to_string(file_path):
+    """
+    Translates the whole pdf file to text. Utilizes Xpdf.
+    NOTICE: Because of the pdf format page index starts at 1.
+    :param file_path: Path to the pdf file
+    :return: String containing the text in the pdf file
+    """
+
+    # Program and it's arguments that get called here pdftotext from xpdf
+    cmd = ["pdftotext", '-layout', '-enc', 'UTF-8', file_path, '-']
+
+    # Opens a new process, connect to it's stdout, then read and return the output
+    return subprocess.Popen(cmd,
+                            encoding='utf-8',
+                            stdout=subprocess.PIPE,
+                            stdin=subprocess.PIPE,
+                            stderr=subprocess.PIPE).communicate()[0]
+
+
+def pdf_string_to_pages(pdf_string):
+    """
+    Breaks a pdf that has been converted to text into a list containing strings of each page
+    :param pdf_string:
+    :return:
+    """
+    # TODO documentation and pretty much everything else
+
+
 if __name__ == '__main__':
-    pdf_file = Path('.') /'samples' / '0.pdf'
-    test = str(pdf_file)
-    print(pdf_to_string(test, 17))
+    print(pdf_to_string("samples/0.pdf"))
