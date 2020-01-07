9de39aaac36001456d55a7f42e6c347f12351e69
Aleksi <>
2020-01-07 18:20:44 +0200

    Created functional naive bayes model to recognise pages where the relevant information is found

--- boersebot/modelbuilder.py
+++ boersebot/modelbuilder.py
@@ -3,6 +3,9 @@ import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 import os
 import pickle
+from pathlib import Path
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
 
 
 def tokenize_string(input_string):
@@ -53,8 +56,68 @@ def build_dictionary(samples_location):
     return count_vectorizer
 
 
+def build_training(samples_location, count_vectorizer):
+    """
+    builds a training set based on directory location that has the samples and a labels.csv which contains the
+    label for each file
+    :param samples_location: Directory location of samples and label data
+    :param count_vectorizer: A Scipy countvectorizer that has been fit with build_dictionary method
+    :return: feature vectors and labels for each vector
+    """
+    # Get the list of labels from the samples_location directory
+    with open(samples_location+'/labels.csv') as f:
+        label_list = [int(label) for label in f]
+
+    # Initialize the vectors and labels as numpy arrays for more convenient use later
+    vectors, labels = [], np.array([])
+
+    for file in os.scandir(samples_location):
+        if file.name != 'labels.csv':
+            pdf = pdf_reader.pdf_to_string(file.path)
+            pages = pdf_reader.pdf_to_pages(pdf)
+
+            for page in pages:
+                tokens = " ".join(tokenize_string(page))
+                vectors.append(tokens)
+                """
+                if vectors.size == 0:
+                    vectors = count_vectorizer.transform(tokens)
+                else:
+                    print(count_vectorizer.transform(tokens))
+                    vectors = np.concatenate((vectors, count_vectorizer.transform(tokens)), axis=0)
+                """
+            # Name of each file (without extension) is the index of desired page in the label_list
+            i = int(Path(file.path).stem)
+
+            # Formulate the label vector for the whole document at the same time
+            # Create a zero vector in the length of the pages and switch the correct page to 1
+            er_page = label_list[i]
+            page_labels = np.zeros(len(pages))
+            if i >= 0:  # Files where there are no correct pages are labeled as -1
+                # Take into account that the pdf files are 1-indexed
+                page_labels[er_page-1] = 1
+            labels = np.concatenate((labels, page_labels), axis=None)
+
+    # The transpose for labels is done again for convenience later on
+    return count_vectorizer.transform(vectors), labels
+
+
 if __name__ == '__main__':
     with open('assets/count_vectorizer.pkl', 'rb') as f:
         cv = pickle.load(f)
 
-    print(cv.get_feature_names())
+    # model = MultinomialNB()
+    #
+    # X, y = build_training("samples", cv)
+    # print(y[17])
+    # model.fit(X, y)
+
+    # with open('assets/naive_bayes_clf.pkl', 'wb') as f:
+    #     pickle.dump(model, f)
+
+    with open('assets/naive_bayes_clf.pkl', 'rb') as f:
+        model = pickle.load(f)
+    pdf = pdf_reader.pdf_page_to_string('samples/1.pdf', 14)
+    pdf = " ".join(tokenize_string(pdf))
+    trans = cv.transform([pdf])
+    print(model.predict(cv.transform([pdf])))
